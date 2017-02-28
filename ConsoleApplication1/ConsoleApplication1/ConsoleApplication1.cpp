1a5f6c6a9cb25e13051325ff84ad134e6c136609
hlx <hlx@DESKTOP-LQD20NH>
2017-02-28 23:53:54 +0800

    jinxingceshi

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	int n= 0;
+	for (int i = 0; i < 4; i++)
+	{
+		cout << "test:" << i << endl;
+	}
+	cout << "Successful"<<endl;
+	system("pause");
+    return 0;
+}
+
