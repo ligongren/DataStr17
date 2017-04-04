181e7873220d4e8659da0d73950ca81a161116e4
h <928585672@qq.com>
2017-04-04 15:05:37 +0800

    写完了递归转非递归

--- C6/nonRecursion/nonRecursion/nonRecursion.cpp
+++ C6/nonRecursion/nonRecursion/nonRecursion.cpp
@@ -1,12 +1,11 @@
 // nonRecursion.cpp : 定义控制台应用程序的入口点。
 //
 
+#include "stdafx.h"
+
 //程序功能
 //一个递归程序的等价非递归程序
 //原程序为：
-
-#include "stdafx.h"
-
 //void P(int n)
 //{
 //	if (n > 0)
@@ -16,6 +15,7 @@
 //		P(n - 1);
 //	}
 //}
+
 int main()
 {
 	int stack[100];
@@ -28,47 +28,39 @@ int main()
 	int nReal = n;
 
 L0:
-	if (n>0)
+	if (nReal > 0)
 	{
-		cout << n;
-		
-		stack[top++] = n;
-		n = n - 1;
-		goto L0;
+		cout << nReal << " ";
 
-		stack[top++] = n;
-		n = n - 1;
+		stack[++top] = nReal;
+		stack[++top] = 1;
+		nReal--;
 		goto L0;
+	L1:
+		nReal = stack[top--];
+
+		stack[++top] = nReal;
+		stack[++top] = 2;
+		nReal--;
+		goto L0;
+	L2:
+		nReal = stack[top--];
 	}
-	if (top != )
+
+	if (top != 1)
 	{
-		n = stack[top--];
-		goto L0;
+		switch (stack[top--])
+		{
+		case 1:
+			goto L1;
+			break;
+		case 2:
+			goto L2;
+			break;
+		}
 	}
 
-//L0:
-//
-//	if (nReal > 0)
-//	{
-//		cout << nReal<<" ";
-//
-//		stack[++top] = nReal;
-//		nReal = nReal - 1;
-//		goto L0;
-//
-//		stack[++top] = nReal;
-//		nReal = nReal - 1;
-//		goto L0;
-//	}
-//
-//	if (top != 0)
-//		n = stack[top--];
-//		goto L0;
-
-
-
-//
-//	cout << endl;
-//	system("pause");
+	cout << endl;
+	system("pause");
 	return 0;
 }
