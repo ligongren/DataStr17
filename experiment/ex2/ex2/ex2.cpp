ac89ce4f9d431b3d886c6b8ec89134b3c212c783
h <928585672@qq.com>
2017-05-10 13:00:16 +0800

    开始写哈夫曼树

--- experiment/ex2/ex2/ex2.cpp
+++ experiment/ex2/ex2/ex2.cpp
@@ -6,6 +6,7 @@
 
 
 #define eleType char
+#define limit 100
 
 //#include <fstream>
 //ifstream fin("in.txt");
@@ -18,14 +19,20 @@ int main()
 
 	A.initial();
 
-	A.preorder(A.root);
-	cout << endl;
-	
-	A.inorder(A.root);
-	cout << endl;
+	//A.preorder(A.root);
+	//cout << endl;
+	//
+	//A.inorder(A.root);
+	//cout << endl;
 
-	A.postorder(A.root);
-	cout << endl;
+	//A.postorder(A.root);
+	//cout << endl;
+
+	eleType data;
+	cin >> data;
+	A.find(A.root, data);
+	A.include();
+	
 
 	system("pause");
     return 0;
---
+++ experiment/ex3/ex3/ex3.cpp
@@ -0,0 +1,17 @@
+// ex3.cpp : 定义控制台应用程序的入口点。
+//
+
+//程序功能：实现哈夫曼树
+
+#include "stdafx.h"
+
+#define eleType char
+#define limit 100
+
+#include "ex2.h"
+
+int main()
+{
+    return 0;
+}
+
