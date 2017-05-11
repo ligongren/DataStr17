976bf0560fc802635391cd8baa4a25cb55aa7fa8
h <928585672@qq.com>
2017-05-11 12:21:13 +0800

    哈夫曼树

--- experiment/ex3/ex3/ex3.cpp
+++ experiment/ex3/ex3/ex3.cpp
@@ -5,13 +5,24 @@
 
 #include "stdafx.h"
 
-#define eleType char
+#define eleType int
 #define limit 100
 
 #include "ex2.h"
+#include "hTree.h"
 
 int main()
 {
+	hTree tree;
+	int a[] = { 3,4,5,6,8,10,12,18 };
+	for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+	{
+		tree.in(a[i]);
+	}
+	tree.build();
+	tree.out();
+
+	system("pause");
     return 0;
 }
 
