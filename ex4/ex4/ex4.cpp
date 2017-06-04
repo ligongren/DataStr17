43e8ac08d18fa5cbba8c09f0b4cab29475c27d65
h <928585672@qq.com>
2017-06-04 13:44:10 +0800

    建立python实验项目

---
+++ ex4/ex4/ex4.cpp
@@ -0,0 +1,20 @@
+// ex4.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+#include <consoleapi.h>
+
+int f()
+{
+	return 0;
+}
+
+int main()
+{
+	int a = 0;
+	printf("Hello World!");
+	f();
+    return 0;
+}
+
