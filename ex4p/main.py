b0ec034b4e6e4eeb3ac48c37e5425a874227e0b0
h <928585672@qq.com>
2017-06-13 22:50:36 +0800

    写完了邻接链表实现深度遍历

--- ex4p/main.py
+++ ex4p/main.py
@@ -1,3 +1,17 @@
-import traverse
+from traverse import *
 
-traverse.hello()
+build(1, [2, 7])
+build(2, [1, 3, 4])
+build(3, [2])
+build(4, [2, 5, 6, 8])
+build(5, [4, 6])
+build(6, [4, 5, 8])
+build(8, [4, 6])
+build(7, [1, 9, 10])
+build(9, [7, 10])
+build(10, [7, 9])
+# 数据在PPT上样例的基础上交换了７和８的位置，为了防止生成树就是数字的顺序
+
+
+
+traverse()
--- ex4p/traverse.py
+++ ex4p/traverse.py
@@ -1,2 +1,47 @@
-def hello():
-    print('Howdy!')
+graph = []
+
+
+def build(node, linked):
+    graph.append([node, linked, False])
+
+
+def traverse():
+    for i in graph:
+        if not i[2]:
+            dfs(i)
+
+
+def dfs(now_node):
+    if now_node[2]:
+        return  # 如果已经遍历过则直接返回
+    now_node[2] = True  # 将状态置为已遍历
+
+    if now_node[0]:
+        # 如果不为0则输出
+        if no_next(now_node):
+            print(now_node[0])
+        else:
+            print(now_node[0], end=" ")
+
+    for i in now_node[1]:
+        if not trave_ed(i):
+            dfs(find_info(i))
+
+
+def no_next(now_node):
+    for i in now_node[1]:
+        if not trave_ed(i):
+            return False
+    return True
+
+
+def trave_ed(number):
+    for i in graph:
+        if i[0] == number:
+            return i[2]
+
+
+def find_info(number):
+    for i in graph:
+        if i[0] == number:
+            return i
